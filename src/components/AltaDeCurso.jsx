ad00281d4d635470efe37d7bc3af0aaa8c6052c0
Teche95 <silencertec@gmail.com>
2023-09-08 20:18:38 -0300

    doc

--- src/components/AltaDeCurso.jsx
+++ src/components/AltaDeCurso.jsx
@@ -1,36 +1,35 @@
 import { useEffect, useState } from "react"
 
+
+/**
+ * Componente AltaDeCurso para la creación de cursos.
+ * @returns {JSX.Element} Elemento JSX que representa el formulario de alta de cursos.
+ */
+
 const AltaDeCurso = () => {
+    // Estado para el ID del curso seleccionado.
     const [idCurso, setIdCurso] = useState("seleccione")
+    // Estado para almacenar los detalles del curso seleccionado.
     const [cursoSelec, setCursoSelec] = useState("")
+    // Estado para almacenar la lista de cursos disponibles.
     const [cursos, setCursos] = useState([])
 
 
-    // const a = cursos.cursos?.filter(el => el.id === parseInt(idCurso))
-    // console.log(a)
-    // console.log(cursos.cursos?.map(el=>el.tipo))
-    // console.log(idCurso)
-    // console.log(idCurso)
-
+    /**
+     * Maneja el cambio en la selección de cursos.
+     * @param {Object} event - Evento de cambio en el select.
+     */
     const handleDescription = (event) => {
         const selectedId = event.target.value;
         if (selectedId === "seleccione") {
+            // Si se selecciona "seleccione", limpiamos los detalles del curso.
             setCursoSelec(null)
         } setIdCurso(selectedId);
     }
 
-    // useEffect(() => {
-    //     if (!cursos.length) {
-    //         const url = `https://backend-iweb.onrender.com/api/v1/cursos`
-    //         fetch(url)
-    //             .then((response) => response.json())
-    //             .then(data => {
-    //                 setCursos(data)
-    //             })
-    //     }
-
-    // }, [])
-
+    /**
+    * Efecto para cargar la lista de cursos desde el servidor al montar el componente.
+    */
     useEffect(() => {
         if (!cursos.length) {
             const url = `https://backend-iweb.onrender.com/api/v1/cursos`;
@@ -55,6 +54,9 @@ const AltaDeCurso = () => {
         }
     }, []);
 
+    /**
+    * Efecto para actualizar el curso seleccionado cuando cambia el ID del curso.
+    */
     useEffect(() => {
         if (idCurso !== "seleccione") {
             const cursoFil = cursos.cursos?.filter(el => el.id === parseInt(idCurso))
@@ -63,25 +65,14 @@ const AltaDeCurso = () => {
 
     }, [idCurso])
 
-    // const handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     const url = `https://backend-iweb.onrender.com/api/v1/cursos`
-    //     fetch(url, {
-    //         method: "POST",
-    //         body: JSON.stringify(cursoSelec?.[0]),
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //     })
-    //         .then((res) => res.json())
-    //         .catch((error) => console.error("Error:", error))
-    //         .then((response) => console.log("Success:", response));
-    // }
-
+    /**
+   * Maneja el envío del formulario para crear un nuevo curso.
+   * @param {Object} e - Evento de envío del formulario.
+   */
     const handleSubmit = async (e) => {
         e.preventDefault();
         const url = `https://backend-iweb.onrender.com/api/v1/cursos`;
-    
+
         try {
             const response = await fetch(url, {
                 method: "POST",
@@ -90,13 +81,14 @@ const AltaDeCurso = () => {
                     "Content-Type": "application/json",
                 },
             });
-    
             if (!response.ok) {
                 throw new Error(`La solicitud POST a ${url} no se completó correctamente.`);
             }
-    
+
             const responseData = await response.json();
             console.log("Success:", responseData);
+            // Muestra la alerta después de que la solicitud se haya completado con éxito.
+            alert("curso creado")
         } catch (error) {
             console.error("Error:", error);
         }
@@ -134,11 +126,3 @@ const AltaDeCurso = () => {
 }
 
 export default AltaDeCurso
-
-// Tareas
-
-// 1. Arreglar este problema: console.js:213 Warning: A component is changing an uncontrolled input to be controlled. This is likely caused by the value changing from undefined to a defined value, which should not happen. Decide between using a controlled or uncontrolled input element for the lifetime of the component. 😄👍
-
-// 2. Hacer funcionalidad de crear curso osea hacer funcionar boton de ingresar. 😄👍
-
-// 3. Modificar la descripción de un curso. 😄👍
